cb8ea662af355573581d0fc3008d61486c645869
ibrahim alzoubi <gusalzib@student.gu.se>
2024-11-21 17:17:05 +0100

    #18 add My Appointments page

--- dentalHeathcareSystem/client_patient/src/router.js
+++ dentalHeathcareSystem/client_patient/src/router.js
@@ -3,11 +3,13 @@ import { createRouter, createWebHistory } from "vue-router";
 import Home from "./views/Home.vue";
 import NewAppointment from "./views/NewAppointment.vue";
 import PrivacyPolicy from "./views/PrivacyPolicy.vue";
+import MyAppointments from "./views/MyAppointments.vue";
 
 const routes = [
   { path: "/", name: "home", component: Home },
   { path: "/new/appointment", name: "newAppointment", component: NewAppointment },
-  {path:'/privacy_policy', name: 'privacy_policy', component: PrivacyPolicy}
+  { path: '/privacy_policy', name: 'privacy_policy', component: PrivacyPolicy },
+  {path: '/my_appointments', name: 'my_appointments', component: MyAppointments}
 
 ];
 
