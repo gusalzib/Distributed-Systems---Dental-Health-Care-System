7dc8075758421197fd0028bcb1194dd6b47bb672
ibrahim alzoubi <gusalzib@student.gu.se>
2024-11-22 08:53:44 +0100

    #18 add registration page and registration form

--- dentalHeathcareSystem/client_patient/src/router.js
+++ dentalHeathcareSystem/client_patient/src/router.js
@@ -5,13 +5,16 @@ import NewAppointment from "./views/NewAppointment.vue";
 import PrivacyPolicy from "./views/PrivacyPolicy.vue";
 import MyAppointments from "./views/MyAppointments.vue";
 import SingleAppointment from "./views/SingleAppointment.vue";
+import Registration from "./views/Registration.vue";
 
 const routes = [
   { path: "/", name: "home", component: Home },
   { path: "/new/appointment", name: "newAppointment", component: NewAppointment },
   { path: '/privacy_policy', name: 'privacy_policy', component: PrivacyPolicy },
   { path: '/my_appointments', name: 'my_appointments', component: MyAppointments },
-  {path: '/single_appointment', name: 'singleAppointment', component: SingleAppointment}
+  { path: '/single_appointment', name: 'singleAppointment', component: SingleAppointment },
+  { path: '/registration', name: 'registration', component: Registration },
+  
 
 ];
 
---
+++ dentalHeathcareSystem/client_patient/src/views/Registration.vue
@@ -0,0 +1,60 @@
+<template>
+  <main>
+        <div class="registration-container">
+            
+            <div class="patient-information-form">
+                <h1>Registration form</h1>
+                <label>Full name:* </label>
+                <input type="text" id="patient-name"  v-model="patient.name" required>
+                <hr>
+                <label>Social Security Number (SSN):* </label>
+                <input type="text" id="patient-ssn" v-model="patient.ssn" required>
+                <hr>
+
+                <label>Phone number:* </label>
+                <input type="text" id="patient-phone" v-model="patient.phone_number" required>
+                <hr>
+
+                <label>Email:* </label>
+                <input type="email" id="patient-email" v-model="patient.email" required>
+                <hr>
+
+                <label>Address:* </label>
+                <input type="address" id="patient-address" v-model="patient.address" required>
+                <hr>
+                <button class="submit-button">Confirm Appointment</button>
+                <router-link to="/privacy_policy">Read about how we handle your information</router-link>
+                <p>Obligatory fields *</p>
+            </div>
+        </div>
+  </main>
+</template>
+
+
+<script>
+// @ is an alias to /src
+import { Api } from '@/Api'
+
+export default {
+  name: 'registration',
+  data() {
+    return {
+        patient: {
+            name: '',
+            email: '',
+            phone_number: '',
+            ssn: '',
+            address: '',
+        },
+    }
+  },
+    mounted() {
+    
+  },
+    methods: {
+        
+  }
+}
+</script>
+
+<style src="../assets/main.css"></style>
