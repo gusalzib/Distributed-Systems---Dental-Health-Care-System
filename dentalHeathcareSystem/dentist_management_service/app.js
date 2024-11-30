56c1bd272dbe721a72b2e1db530d782f6450c9a6
muhamadj <muhamadj@student.chalmers.se>
2024-11-30 10:36:42 +0100

    #9 add dentist management service
    add controller file, appjs runnable on port 3005, and tested already with PUT, POST, GET, GET (all), PUT and DEL via postman

---
+++ dentalHeathcareSystem/dentist_management_service/app.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var mongoose = require('mongoose')
+var cors = require('cors');
+
+var app = express();
+var port = 3005;
+
+var mongoURI =  "mongodb://localhost:27017/dentalHealthcareSystem";
+// Connect to MongoDB
+mongoose
+    .connect(mongoURI)
+    .catch(function (err) {
+        console.error(`Failed to connect to MongoDB with URI: ${mongoURI}`);
+        console.error(err.stack);
+        process.exit(1);
+    })
+    .then(function () {
+        console.log(`Connected to MongoDB with URI: ${mongoURI}`);
+    });
+
+const origins = [
+    "http://localhost:3000",
+];
+app.use(
+    cors({
+        origin: origins, // Allow all origins
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        credentials: true,
+        preflightContinue: true
+    })
+);
+
+
+// Also, handle preflight requests for all routes
+app.options("*", cors());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+const { registerDentist, retrieveDentists, retrieveASpecificDentist, updateDentist, deleteDentistByID } = require("./src/dentist_controller/dentist-controller");
+
+app.post("/api/dentists", registerDentist)
+app.get("/api/dentists", retrieveDentists)
+app.get("/api/dentists/:dentist_id", retrieveASpecificDentist)
+app.put("/api/dentists/:dentist_id", updateDentist)
+app.delete("/api/dentists/:dentist_id", deleteDentistByID)
+
+app.listen(port, function (err) {
+    if (err) throw err;
+    console.log(`Express server listening on port ${port}`);
+    console.log(`Backend: http://localhost:${port}/api/`);
+    console.log(`Frontend (production): http://localhost:${port}/`);
+});
